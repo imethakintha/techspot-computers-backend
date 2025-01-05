6515874bab5bbe6e8654f71400596570463950da
imethakintha <imethgithubofficial@gmail.com>
2025-01-05 19:42:51 +0530

    add user functionalities✅

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,61 @@
 import dotenv from "dotenv";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "../models/user.js";
 
 dotenv.config();
 
-export function registerUser(req, res){
+export function registerUser(req, res) {
+  const data = req.body;
 
+  data.password = bcrypt.hashSync(data.password, 10);
+
+  const newUser = new User(data);
+
+  newUser
+    .save()
+    .then(() => {
+      res.json({ message: "User registered successfully" });
+    })
+    .catch((error) => {
+      res.status(500).json({ message: "Error registering user" });
+    });
+}
+
+export function loginUser(req, res) {
+  const data = req.body;
+
+  User.findOne({
+    email: data.email,
+  }).then((user) => {
+    if (user == null) {
+      res.status(404).json({
+        error: "User not found",
+      });
+    } else {
+      const isPasswordCorrect = bcrypt.compareSync(
+        data.password,
+        user.password
+      );
+
+      if (isPasswordCorrect) {
+        const token = jwt.sign({
+          firstName: ser.firstName,
+          lastName: user.lastName,
+          email: user.email,
+          role: user.role,
+          profilePicture: user.profilePicture,
+        },process.env.JWT_SECRET);
+
+        res.json({
+          message: "Login successful",
+          token: token
+        })
+      }else{
+        res.status(401).json({
+            error: "Login failed"
+        })
+      }
+    }
+  });
 }
--- index.js
+++ index.js
@@ -2,12 +2,29 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+import userRouter from "./routes/userRouter.js";
+
 
 dotenv.config();
 
 let app = express();
 
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  let token = req.headers("Authorization");
+
+  if(token != null){
+    token = token.replace("Bearer ", "");
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if(!err){
+            req.user = decoded;
+        }
+    })
+  }
+  next();
+})
 
 let mongoURL = process.env.MONGO_URL;
 
@@ -18,6 +35,8 @@ const connection = mongoose.connection;
 connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
 });
+
+app.use("/api/users", userRouter);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
