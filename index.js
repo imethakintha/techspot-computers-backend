eeea68158dc61c0c1e1b6033e1aa622ce468eead
imethakintha <imethgithubofficial@gmail.com>
2025-01-05 16:27:26 +0530

    connect to the mongoDB

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 import express from "express";
 import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 let app = express();
+
 app.use(bodyParser.json());
+
+let mongoURL = process.env.MONGO_URL;
+
+mongoose.connect(mongoURL);
+
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
